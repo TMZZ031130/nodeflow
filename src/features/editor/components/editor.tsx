59e809dffd7fbcd86be2cf639f0a563a5e541736
ze <3218208599@qq.com>
2025-12-29 21:32:00 +0800

    feat: init reactflow

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  type Node,
+  type Edge,
+  type NodeChange,
+  type EdgeChange,
+  type Connection,
+  MiniMap,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
 
@@ -11,8 +25,45 @@ export const EditorError = () => {
   return <ErrorView message="Error loading editor" />;
 };
 
-export const Editor = ({ workflowId }: { workflowId: string }) => {
-  const { data: workflow } = useSuspenseWorkflow(workflowId);
+const initialNodes = [
+  { id: "n1", position: { x: 0, y: 0 }, data: { label: "Node 1" } },
+  { id: "n2", position: { x: 0, y: 100 }, data: { label: "Node 2" } },
+];
+const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
 
-  return <p>{JSON.stringify(workflow, null, 2)}</p>;
+export const Editor = ({ workflowId }: { workflowId: string }) => {
+  // const { data: workflow } = useSuspenseWorkflow(workflowId);
+
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params: Connection) =>
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
+  return (
+    <div className="size-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      />
+      <MiniMap nodeStrokeWidth={3} />
+    </div>
+  );
 };
