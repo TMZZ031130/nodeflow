efbfae0eb609062f2ac3e56b75b05f39e98f94bd
ze <3218208599@qq.com>
2026-02-27 22:53:41 +0800

    feat: add credential schema and routes

---
+++ src/features/credentials/hooks/use-credentials.ts
@@ -0,0 +1,90 @@
+import { useTRPC } from "@/trpc/client";
+import {
+  useMutation,
+  useQuery,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useCredentialsParams } from "./use-credentials-params";
+import type { CredentialType } from "@/generated/prisma/enums";
+
+export const useSuspenseCredentials = () => {
+  const trpc = useTRPC();
+  const [params] = useCredentialsParams();
+
+  return useSuspenseQuery(trpc.credentials.getMany.queryOptions(params));
+};
+
+export const useCreateCredential = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  return useMutation(
+    trpc.credentials.create.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Credential "${data.name}" created`);
+        queryClient.invalidateQueries(
+          trpc.credentials.getMany.queryOptions({}),
+        );
+      },
+      onError: () => {
+        toast.error("Credential created fail");
+      },
+    }),
+  );
+};
+
+export const useRemoveCredential = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.credentials.remove.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Credential "${data.name}" removed`);
+        queryClient.invalidateQueries(
+          trpc.credentials.getMany.queryOptions({}),
+        );
+        queryClient.invalidateQueries(
+          trpc.credentials.getOne.queryFilter({ id: data.id }),
+        );
+      },
+    }),
+  );
+};
+
+export const useSuspenseCredential = (id: string) => {
+  const trpc = useTRPC();
+  return useSuspenseQuery(
+    trpc.credentials.getOne.queryOptions({
+      id,
+    }),
+  );
+};
+
+export const useUpdateCredential = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.credentials.update.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Credential ${data.name} saved`);
+        queryClient.invalidateQueries(
+          trpc.credentials.getMany.queryOptions({}),
+        );
+        queryClient.invalidateQueries(
+          trpc.credentials.getOne.queryOptions({ id: data.id }),
+        );
+      },
+      onError: (error) => {
+        toast.error(`Failed to save credential: ${error.message}`);
+      },
+    }),
+  );
+};
+
+export const useCredentialsByType = (type: CredentialType) => {
+  const trpc = useTRPC();
+  return useQuery(trpc.credentials.getByType.queryOptions({ type }));
+};
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
+import { credentialRouter } from "@/features/credentials/server/routers";
 import { createTRPCRouter } from "../init";
 import { workflowRouter } from "@/features/workflows/server/routes";
 
 export const appRouter = createTRPCRouter({
   workflows: workflowRouter,
+  credentials: credentialRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
